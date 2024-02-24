ed1bdc8a72cf2c50a217d6de4c0e3bf6e5f88822
Tavleen0302 <tavleen0302@gmail.com>
2024-02-24 17:03:06 -0500

    change

--- MediLink/App.js
+++ MediLink/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Image } from 'react-native';
-import PatientSignUp from './Apps/Screens/PatientSignLoginButtons/PatientSignUp';
-import PatientLogin from './Apps/Screens/PatientSignLoginButtons/PatientLogin';
+import PatientSignUp from './Apps/Screens/PatientSignLoginButtons/PatientSignUpForm';
+import PatientLogin from './Apps/Screens/PatientSignLoginButtons/PatientLoginForm';
 
 export default function App() {
   return (
---
+++ MediLink/Apps/Screens/PatientSignLoginButtons/PatientButton.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function PatientButton() {
+  return (
+    <View>
+      <Text>PatientButton</Text>
+    </View>
+  )
+}
