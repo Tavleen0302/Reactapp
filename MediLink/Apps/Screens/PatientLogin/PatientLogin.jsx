cc453c87675f3160f08d2a9ef499e50fb8792f0b
Tavleen0302 <tavleen0302@gmail.com>
2024-02-24 16:19:58 -0500

    added buttons

--- MediLink/Apps/Screens/PatientLogin/PatientLogin.jsx
+++ MediLink/Apps/Screens/PatientLogin/PatientLogin.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import {View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
 export default function PatientLogin() {
   const handleLoginPress = () => {
@@ -8,17 +8,25 @@ export default function PatientLogin() {
   };
 
   return (
-    <TouchableOpacity onPress={handleLoginPress} style={styles.button}>
-      <Text style={styles.buttonText}>Login</Text>
-    </TouchableOpacity>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={handleLoginPress} style={styles.button}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   button: {
-    backgroundColor: 'green', // Different color for login button
+    backgroundColor: 'blue',
     padding: 15,
     borderRadius: 8,
+    marginTop: 20,
   },
   buttonText: {
     color: 'white',
