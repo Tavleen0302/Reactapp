805152903acab5a0e18d07488a72c7c9e20cc742
mithunan4 <mithunan70@icloud.com>
2024-02-24 16:58:00 -0500

    home page

--- MediLink/Apps/Screens/Homepage/home.jsx
+++ MediLink/Apps/Screens/Homepage/home.jsx
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from 'react-native';
+import React from 'react';
 
-export default function home() {
+export default function Home({ navigation }) {
   return (
-    <View>
-      <Text>home</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Welcome to the Home Screen</Text>
+      <View style={{ marginTop: 20 }}>
+        <Button
+          title="I'm a Medical Professional"
+          onPress={() => navigation.navigate('DoctorScreen')}
+        />
+      </View>
+      <View style={{ marginTop: 10 }}>
+        <Button
+          title="I'm a Patient"
+          onPress={() => navigation.navigate('PatientScreen')}
+        />
+      </View>
     </View>
-  )
+  );
 }
