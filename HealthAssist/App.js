914f2b8fa99f349bca773edc712bb7c23d8730ba
Tavleen0302 <tavleen0302@gmail.com>
2024-02-24 18:21:30 -0500

    change

--- HealthAssist/App.js
+++ HealthAssist/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Home from './App/Screen/Home/home';
 import Patientbuttons from './App/Screen/Patient/Patientbuttons';
 import Doctorbuttons from './App/Screen/Doctor/Doctorbuttons';
 
-const Stack = createStackNavigator();
-
 export default function App() {
+  const [screen, setScreen] = useState('Home');
+
+  const handleScreenChange = (newScreen) => {
+    setScreen(newScreen);
+  };
+
   return (
     <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Patientbuttons" component={Patientbuttons} />
-          <Stack.Screen name="Doctorbuttons" component={Doctorbuttons} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      {screen === 'Home' && <Home setScreen={handleScreenChange} />}
+      {screen === 'Patientbuttons' && <Patientbuttons />}
+      {screen === 'Doctorbuttons' && <Doctorbuttons />}
       <StatusBar style="auto" />
     </View>
   );
